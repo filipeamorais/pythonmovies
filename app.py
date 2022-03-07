73cb77181a3be535dff4a9e9e51df7a615ec2f26
Filipe Morais <afilipem@gmail.com>
2022-03-06 19:05:14 -0600

    adds fucntion to select a tabke on the elephantsql

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 import psycopg2
+
+url = "postgres://xbsahsxo:q8KLP4sf2WZ6uY7P-zAxSiMnwzFh67FN@kashin.db.elephantsql.com/xbsahsxo"
+connection = psycopg2.connect(url)
+
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM users;")
+first_user = cursor.fetchone()
+
+print(first_user)
+
+connection.close()
+
